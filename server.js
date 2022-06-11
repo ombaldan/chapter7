4f52ef12eccab2a830febc025e69a6efcbb4b2e5
ombaldan <ombaldan@gmail.com>
2022-06-11 23:31:25 +0800

    Modified Router and Server

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const flash = require('express-flash');
-const PORT = 8000
+const PORT = 8000;
 
-app.get('/', (req, res) => {
-    res.send('masuk');
-});
+app.use(express.urlencoded({extended: false}));
+
+app.use(session({
+    secret: "Ini Rahasia ya",
+    resave: false,
+    saveUninitialized: false
+}));
+
+const router = require('./router');
+
+app.use(router);
+
 app.listen(PORT, () => {
-    console.log(`Server sudah running`);
+    console.log(`Server sudah running di port ${PORT}`);
 });
